290bc3036acd90029749eb11073ae75cf3fb70a5
LizandroV <lizandrovivanco16@gmail.com>
2024-06-24 17:35:05 -0500

    individual activity 2

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,8 @@
 import ProductData from "./ProductData.mjs";
-import ProductListing from "./ProductList.mjs";
-import { getParam } from "./utils.mjs";2
+import ProductList from "./ProductList.mjs";
 
 const dataSource = new ProductData("tents");
-const productId = getParam("product");
+const element = document.querySelector(".product-list");
+const listing = new ProductList("Tents", dataSource, element);
 
-const product = new ProductListing(productId, dataSource);
-product.init();
+listing.init();
